3b0307691a76d49effada123a1aeb03fd51c1b14
Eleonardo.Oliveira <eleonardo.oliveira@zup.com.br>
2021-05-25 12:23:56 -0300

    - Melhorias nos testes

--- src/test/kotlin/br/com/zup/edu/pix/registra/RegistraChaveEndpointTest.kt
+++ src/test/kotlin/br/com/zup/edu/pix/registra/RegistraChaveEndpointTest.kt
@@ -4,27 +4,44 @@ import br.com.zup.edu.grpc.KeymanagerRegistraGrpcServiceGrpc
 import br.com.zup.edu.grpc.RegistraChavePixRequest
 import br.com.zup.edu.grpc.TipoDeChave
 import br.com.zup.edu.grpc.TipoDeConta
+import br.com.zup.edu.integration.itau.ContasDeClientesNoItauClient
+import br.com.zup.edu.integration.itau.DadosDaContaResponse
+import br.com.zup.edu.integration.itau.InstituicaoResponse
+import br.com.zup.edu.integration.itau.TitularResponse
 import br.com.zup.edu.pix.ChavePixRepository
+import br.com.zup.edu.pix.ContaAssociada
 import io.grpc.Channel
 import io.grpc.Status
 import io.grpc.StatusRuntimeException
 import io.micronaut.context.annotation.Factory
 import io.micronaut.grpc.annotation.GrpcChannel
 import io.micronaut.grpc.server.GrpcServerChannel
+import io.micronaut.http.HttpResponse
+import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.util.*
-import javax.inject.Singleton
 import org.junit.jupiter.api.assertThrows
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import java.util.*
+import javax.inject.Inject
+import javax.inject.Singleton
 
 @MicronautTest(transactional = false)
 internal class RegistraChaveEndpointTest(
     private val repository: ChavePixRepository,
     private val grpcClient: KeymanagerRegistraGrpcServiceGrpc.KeymanagerRegistraGrpcServiceBlockingStub,
 ) {
+
+    @Inject
+    lateinit var itauClient: ContasDeClientesNoItauClient
+
+    companion object {
+        val CLIENTE_ID: UUID = UUID.randomUUID()
+    }
 
     @BeforeEach
     internal fun setup() {
@@ -41,11 +58,14 @@ internal class RegistraChaveEndpointTest(
         // ########## preparação ##########
         val request: RegistraChavePixRequest = RegistraChavePixRequest
             .newBuilder()
-            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setClienteId(CLIENTE_ID.toString())
             .setTipoDeChave(TipoDeChave.CPF)
             .setChave("02110126108")
             .setTipoDeConta(TipoDeConta.CONTA_CORRENTE)
             .build()
+
+        `when`(itauClient.buscaContaPorTipo(clienteId = CLIENTE_ID.toString(), tipo = "CONTA_CORRENTE"))
+            .thenReturn(HttpResponse.ok(dadosDaContaResponse()))
 
         // ########## ação ##########
         val response = grpcClient.registra(request)
@@ -54,6 +74,7 @@ internal class RegistraChaveEndpointTest(
         with(response) {
             assertNotNull(pixId)
             assertNotNull(clienteId)
+            assertEquals(CLIENTE_ID.toString(), clienteId)
             assertTrue(repository.existsById(UUID.fromString(pixId)))
         }
         assertEquals(1, repository.findAll().size)
@@ -64,11 +85,14 @@ internal class RegistraChaveEndpointTest(
         // ########## preparação ##########
         val request: RegistraChavePixRequest = RegistraChavePixRequest
             .newBuilder()
-            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setClienteId(CLIENTE_ID.toString())
             .setTipoDeChave(TipoDeChave.CELULAR)
             .setChave("+5562996368679")
             .setTipoDeConta(TipoDeConta.CONTA_CORRENTE)
             .build()
+
+        `when`(itauClient.buscaContaPorTipo(clienteId = CLIENTE_ID.toString(), tipo = "CONTA_CORRENTE"))
+            .thenReturn(HttpResponse.ok(dadosDaContaResponse()))
 
         // ########## ação ##########
         val response = grpcClient.registra(request)
@@ -87,11 +111,14 @@ internal class RegistraChaveEndpointTest(
         // ########## preparação ##########
         val request: RegistraChavePixRequest = RegistraChavePixRequest
             .newBuilder()
-            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setClienteId(CLIENTE_ID.toString())
             .setTipoDeChave(TipoDeChave.EMAIL)
             .setChave("eleonardo.ro@gmail.com")
             .setTipoDeConta(TipoDeConta.CONTA_CORRENTE)
             .build()
+
+        `when`(itauClient.buscaContaPorTipo(clienteId = CLIENTE_ID.toString(), tipo = "CONTA_CORRENTE"))
+            .thenReturn(HttpResponse.ok(dadosDaContaResponse()))
 
         // ########## ação ##########
         val response = grpcClient.registra(request)
@@ -110,10 +137,13 @@ internal class RegistraChaveEndpointTest(
         // ########## preparação ##########
         val request: RegistraChavePixRequest = RegistraChavePixRequest
             .newBuilder()
-            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setClienteId(CLIENTE_ID.toString())
             .setTipoDeChave(TipoDeChave.ALEATORIA)
             .setTipoDeConta(TipoDeConta.CONTA_CORRENTE)
             .build()
+
+        `when`(itauClient.buscaContaPorTipo(clienteId = CLIENTE_ID.toString(), tipo = "CONTA_CORRENTE"))
+            .thenReturn(HttpResponse.ok(dadosDaContaResponse()))
 
         // ########## ação ##########
         val response = grpcClient.registra(request)
@@ -154,7 +184,7 @@ internal class RegistraChaveEndpointTest(
         // ########## preparação ##########
         val request: RegistraChavePixRequest = RegistraChavePixRequest
             .newBuilder()
-            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setClienteId(CLIENTE_ID.toString())
             .setTipoDeChave(TipoDeChave.CPF)
             .setChave("02110126109")
             .setTipoDeConta(TipoDeConta.CONTA_CORRENTE)
@@ -177,7 +207,7 @@ internal class RegistraChaveEndpointTest(
         // ########## preparação ##########
         val request: RegistraChavePixRequest = RegistraChavePixRequest
             .newBuilder()
-            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setClienteId(CLIENTE_ID.toString())
             .setTipoDeChave(TipoDeChave.CELULAR)
             .setChave("+62")
             .setTipoDeConta(TipoDeConta.CONTA_CORRENTE)
@@ -200,7 +230,7 @@ internal class RegistraChaveEndpointTest(
         // ########## preparação ##########
         val request: RegistraChavePixRequest = RegistraChavePixRequest
             .newBuilder()
-            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setClienteId(CLIENTE_ID.toString())
             .setTipoDeChave(TipoDeChave.EMAIL)
             .setChave("eleonardo.rogmail.com")
             .setTipoDeConta(TipoDeConta.CONTA_CORRENTE)
@@ -223,7 +253,7 @@ internal class RegistraChaveEndpointTest(
         // ########## preparação ##########
         val request: RegistraChavePixRequest = RegistraChavePixRequest
             .newBuilder()
-            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setClienteId(CLIENTE_ID.toString())
             .setTipoDeChave(TipoDeChave.ALEATORIA)
             .setChave("eleonardo.ro@gmail.com")
             .setTipoDeConta(TipoDeConta.CONTA_CORRENTE)
@@ -242,37 +272,17 @@ internal class RegistraChaveEndpointTest(
     }
 
     @Test
-    fun `nao deve criar chave quando passar um id de Cliente valido mas nao cadastrado no Itau`() {
+    fun `nao deve criar chave quando cliente Id esta valido mas nao cadastrado no Itau ou nao tem essa conta`() {
         // ########## preparação ##########
         val request: RegistraChavePixRequest = RegistraChavePixRequest
             .newBuilder()
-            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157891")
+            .setClienteId(CLIENTE_ID.toString())
             .setTipoDeChave(TipoDeChave.ALEATORIA)
             .setTipoDeConta(TipoDeConta.CONTA_CORRENTE)
             .build()
 
-        // ########## ação ##########
-        val error = assertThrows<StatusRuntimeException> {
-            grpcClient.registra(request)
-        }
-
-        // ########## validação ##########
-        with(error) {
-            assertEquals(Status.FAILED_PRECONDITION.code, status.code)
-            assertEquals("Cliente não encontrado no Itau", status.description)
-        }
-    }
-
-    @Test
-    fun `nao deve criar chave quando cliente id esta certo mas nao tem a conta do tipo especificado`() {
-        // ########## preparação ##########
-        val request: RegistraChavePixRequest = RegistraChavePixRequest
-            .newBuilder()
-            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157890")
-            .setTipoDeChave(TipoDeChave.CPF)
-            .setChave("02110126108")
-            .setTipoDeConta(TipoDeConta.CONTA_POUPANCA)
-            .build()
+        `when`(itauClient.buscaContaPorTipo(clienteId = CLIENTE_ID.toString(), tipo = "CONTA_CORRENTE"))
+            .thenReturn(HttpResponse.notFound())
 
         // ########## ação ##########
         val error = assertThrows<StatusRuntimeException> {
@@ -280,7 +290,7 @@ internal class RegistraChaveEndpointTest(
         }
 
         // ########## validação ##########
-        with(error) {
+        with(error){
             assertEquals(Status.FAILED_PRECONDITION.code, status.code)
             assertEquals("Cliente não encontrado no Itau", status.description)
         }
@@ -291,7 +301,7 @@ internal class RegistraChaveEndpointTest(
         // ########## preparação ##########
         val request: RegistraChavePixRequest = RegistraChavePixRequest
             .newBuilder()
-            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setClienteId(CLIENTE_ID.toString())
             .setTipoDeChave(TipoDeChave.CPF)
             .setChave("02110126108")
             .build()
@@ -313,7 +323,7 @@ internal class RegistraChaveEndpointTest(
         // ########## preparação ##########
         val request: RegistraChavePixRequest = RegistraChavePixRequest
             .newBuilder()
-            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setClienteId(CLIENTE_ID.toString())
             .setChave("02110126108")
             .setTipoDeConta(TipoDeConta.CONTA_CORRENTE)
             .build()
@@ -366,15 +376,18 @@ internal class RegistraChaveEndpointTest(
     }
 
     @Test
-    fun `deve criar chave repetida`() {
+    fun `nao deve criar chave repetida`() {
         // ########## preparação ##########
         val request: RegistraChavePixRequest = RegistraChavePixRequest
             .newBuilder()
-            .setClienteId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setClienteId(CLIENTE_ID.toString())
             .setTipoDeChave(TipoDeChave.CPF)
             .setChave("02110126108")
             .setTipoDeConta(TipoDeConta.CONTA_CORRENTE)
             .build()
+
+        `when`(itauClient.buscaContaPorTipo(clienteId = CLIENTE_ID.toString(), tipo = "CONTA_CORRENTE"))
+            .thenReturn(HttpResponse.ok(dadosDaContaResponse()))
 
         // ########## ação ##########
         grpcClient.registra(request)
@@ -388,12 +401,28 @@ internal class RegistraChaveEndpointTest(
             assertEquals("Chave Pix '02110126108' existente", status.description)
         }
     }
-}
 
-@Factory
-class Chaves {
-    @Singleton
-    fun blockingStub(@GrpcChannel(GrpcServerChannel.NAME) channel: Channel): KeymanagerRegistraGrpcServiceGrpc.KeymanagerRegistraGrpcServiceBlockingStub {
-        return KeymanagerRegistraGrpcServiceGrpc.newBlockingStub(channel)
+    private fun dadosDaContaResponse(): DadosDaContaResponse {
+        return DadosDaContaResponse(
+            tipo = "CONTA_CORRENTE",
+            instituicao = InstituicaoResponse("UNIBANCO ITAU SA", ContaAssociada.ITAU_UNIBANCO_ISPB),
+            agencia = "1218",
+            numero = "291900",
+            titular = TitularResponse("Rafael Ponte", "63657520325")
+        )
+    }
+
+    @MockBean(ContasDeClientesNoItauClient::class)
+    fun itauClient(): ContasDeClientesNoItauClient? {
+        return Mockito.mock(ContasDeClientesNoItauClient::class.java)
+    }
+
+    @Factory
+    class Chaves {
+        @Singleton
+        fun blockingStub(@GrpcChannel(GrpcServerChannel.NAME) channel: Channel): KeymanagerRegistraGrpcServiceGrpc.KeymanagerRegistraGrpcServiceBlockingStub {
+            return KeymanagerRegistraGrpcServiceGrpc.newBlockingStub(channel)
+        }
     }
 }
+
